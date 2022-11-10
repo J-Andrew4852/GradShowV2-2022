b1001c23e2639dbcc4a7e189e647aeadc598ee52
J-Andrew4852 <james.andrew4852@gmail.com>
2022-11-10 15:13:44 +0800

    favicon in

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Layout } from '../components/Layout/Layout'
 import '../styles/globals.css'
 import Script from 'next/script'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -18,6 +19,15 @@ function MyApp({ Component, pageProps }) {
         userFeed.run();
       `}
     </Script>
+    <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon_package_v0.16/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon_package_v0.16/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon_package_v0.16/favicon-16x16.png"/>
+      <link rel="manifest" href="/public/favicon_package_v0.16/site.webmanifest"/>
+      <link rel="mask-icon" href="/public/favicon_package_v0.16/safari-pinned-tab.svg" color="#5bbad5"/>
+      <meta name="msapplication-TileColor" content="#da532c"/>
+      <meta name="theme-color" content="#fffbf4"/>
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
