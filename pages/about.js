1568c5dcf1fbce6cb63175d02a8f2362c22fbed5
JimmyC4 <jameskang99@outlook.com>
2022-11-14 00:26:58 +0800

    not losing all my code again

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import introImage from '../public/assets/images/About/intro-image.jpg'
+import signature from '../public/assets/images/About/signature.png'
+import sparkle from '../public/assets/images/About/sparkle.png'
+import eye from '../public/assets/images/About/eye.png'
+import world from '../public/assets/images/About/world.png'
+import space from '../public/assets/images/About/space.png'
+import team from '../public/assets/images/About/team.png'
 import { committeesData, acknowledgementsData } from '../Utils/creditsData'
 
 // COMPONENT IMPORTS ==================================================================== // 
@@ -33,42 +39,58 @@ export default function About() {
 
       {/* INTRODUCTION */}
       <section className={`${styles.about}`}>
-        <StyledHeader lineOneText="An exhibition" lineTwoText="to remember" sectionNumber='01' />
-
-        <div className={styles.about_group}>
-          <div className={styles.about_introCopy}>
-            <p className={`text-reg left-align body-copy`}>
-              Such a great show wow we have so much to talk about including this this and that, and let’s not forget that. Come see our work and meet great design students from digital design, animation game design and graphic design.
-            </p>
-            <p>
-              More text can go in here that we can break up.Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus ullamcorper ligula ac odio
-            </p>
+        <div className={styles.section_heading}>
+          <div className={styles.elements}>
+            <h2>THE MISSION</h2>
+            <h1>MAKING HISTORY</h1>
+            <span>01</span>
           </div>
 
-          <div className={styles.about_img}>
-            <Image src={introImage} alt="Logistics Meeting Team" layout='responsive' priority={true} placeholder='blur' width={450} height={300} />
-            <div className={styles.imageFooter}>
-              <p className='caption'>12-10-22 : Fun kind spacey(?) themed caption to go here </p>
-            </div>
+          <div className={styles.forward}>
+            <p><strong>FOREWARD</strong></p>
+            <p>Such a great show wow we have so much to talk about including this this and that, and that ofcourse. Come see our work and meet great design students from digital design, animation game design and graphic design. A lot more text will go here, I hope someone actually replaces this with relevant information because it’d be quite embarrassing to have this on our website</p>
           </div>
+          <div className={styles.about_signature}><Image src={signature} lassName={styles.signature} alt="2022 Design Graduates Signature" /></div>
+          
         </div>
       </section>
 
-      {/* WHAT MAKES US WHO WE ARE */}
+      {/* WHO WE ARE */}
       <section className={`${styles.whoweare}`}>
-        <StyledHeader lineOneText="What makes us" lineTwoText="who we are" sectionNumber='02' />
+        <div className={styles.elements}>
+            <h2>WHAT MAKES US</h2>
+            <h1>WHO WE ARE</h1>
+            <span>02</span>
+          </div>
+
+          <div className={styles.whoweare_content}>
+
+            <div className={styles.whoweare_text}>
+              <div className={styles.whoweare_first}>
+                <div className={styles.whoweare_heading}><Image src={sparkle} lassName={styles.signature} alt="Sparkle" /><h1>First Value</h1></div>
+                <p>Something relevant about our second value or whatever. Something sophisticated and smart sounding. Another sentence of sounding smart</p>
+              </div>
+
+              <div className={styles.whoweare_second}>
+                <div className={styles.whoweare_heading}><Image src={eye} lassName={styles.signature} alt="Eye" /><h1>First Value</h1></div>
+                <p>Something relevant about our second value or whatever. Something sophisticated and smart sounding. Another sentence of sounding smart</p>
+              </div>
+
+              <div className={styles.whoweare_third}>
+                <div className={styles.whoweare_heading}><Image src={world} lassName={styles.signature} alt="World" /><h1>First Value</h1></div>
+                <p>Something relevant about our second value or whatever. Something sophisticated and smart sounding. Another sentence of sounding smart</p>
+              </div>
+            </div>
+
+            <div className={styles.whoweare_polaroids}>
+              <Image src={space} lassName={styles.signature} alt="Space" />
+              <Image src={team} lassName={styles.signature} alt="Team" />
+            </div>
+
+            
+          </div>
+
       </section>
-
-      <div className={styles.values}>
-        <div className={styles.value_group}>
-        </div>
-
-        <div className={styles.value_group}>
-        </div>
-
-        <div className={styles.value_group}>
-        </div>
-      </div>
 
 
       {/* THE TEAM BEHIND IT ALL */}
