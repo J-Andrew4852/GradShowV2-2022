3d87158fcfbc6da737ec7255be65e34647cc20d1
J-Andrew4852 <james.andrew4852@gmail.com>
2022-11-14 19:18:27 +0800

    side-folder-minimum

--- components/sideFolder/SideFolder.js
+++ components/sideFolder/SideFolder.js
@@ -64,9 +64,7 @@ export const GradFolder = ({ data, onClose }) => {
                         </div>
 
                         <div className={styles.shortBio}>
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-
-                            {/* {data.bio} */}
+                            {data.bio}
                         </div>
 
                         <div className={styles.linksBar}>
