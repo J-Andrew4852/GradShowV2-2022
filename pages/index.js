f47c54c9e781fc129613d9d95cfdf602a89e7669
J-Andrew4852 <james.andrew4852@gmail.com>
2022-11-18 21:24:19 +0800

    latest-react-version

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script'
+import { version } from 'react'
 
 // COMPONENT IMPORTS ==================================================================== // 
 import animationShowreel from '../public/assets/images/Showreel1.png'
@@ -33,7 +34,7 @@ Description:
 */
 
 export default function Home() {
-
+  console.log(version)
   return (
     <div className={styles.container}>
       <Head>
