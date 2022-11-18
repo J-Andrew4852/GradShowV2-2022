520ae09aebd935c0c33c83ee772906264fb38858
JimmyC4 <jameskang99@outlook.com>
2022-11-18 22:35:30 +0800

    added frontlines credits

--- pages/about.js
+++ pages/about.js
@@ -117,14 +117,134 @@ export default function About() {
 
         <div className={styles.creditsContainer}>
 
-          {acknowledgementsData.map(committee => (
+          {/* {acknowledgementsData.map(committee => (
             <CreditsList key={committee.id} title={committee.name} members={committee.members} />
           ))}
           {acknowledgementsData.map(committee => (
             <CreditsList key={committee.id} title={committee.name} members={committee.members} />
-          ))}
+          ))} */}
+          
+          <div className={styles.column1}> {/* ---COLUMN1--- */}
+            <ul className={styles.front_line_team}>
+                <h1>Brand Identity</h1>
+                <li>Anabel Cressie - Graphic Designer</li>
+                <li>Lilybelle Tarr - Graphic Designer</li>
+                <li>Thomas Earnshaw - Graphic Designer</li>
+                <li>Erin Cunningham - Graphic Designer</li>
+              </ul>
 
+              <ul className={styles.front_line_team}>
+                <h1>Website Leads</h1>
+                <li>Logan Trundle - Digital Lead</li>
+                <li>Anthony Robinson - Digital Co-Lead</li>
+                <li>James Andrew - Frontend Lead</li>
+                <li>Ashleigh Sayers - Backend Lead</li>
+                <li>Salma Jarrar - Design Lead</li>
+              </ul>
+
+              <ul className={styles.front_line_team}>
+                <h1>Showreel Leads</h1>
+                <li>Macgregor Lawson - Video Lead</li>
+                <li>Campbell McCracken - Video Co-lead</li>
+                <li>Max Wright - 3D Lead</li>
+                <li>Flynn Denby - 2D Lead</li>
+              </ul>
+
+              <ul className={styles.front_line_team}>
+                <h1>Instagram</h1>
+                <li>Thomas Earnshaw - Content Strategy</li>
+                <li>Rein Aspinall - Content Strategy</li>
+                <li>Lilybelle Tarr - Content Designer</li>
+                <li>Anabel Cressie - Content Designer</li>
+                <li>Erin Cunningham - Content Designer</li>
+              </ul>
+
+              <ul className={styles.front_line_team}>
+                <h1>Photography</h1>
+                <li>Rein Aspinall - Creative Director</li>
+                <li>Johnson Nguyen - Photographer</li>
+                <li>Thomas Earnshaw - Photographer</li>
+              </ul>
+            </div>
+          
+          <div className={styles.column2}> {/* ---COLUMN2--- */}
+            <ul className={styles.front_line_team}>
+                <h1>Website Development</h1>
+                <li>James Andrew - Frontend Developer</li>
+                <li>Logan Trundle - Frontend Developer</li>
+                <li>Rachael Moriconi - Frontend Developer</li>
+                <li>James Kang - Frontend Developer</li>
+                <li>Jade Mitchell - Frontend Developer</li>
+                <li>Ben Crook - Frontend Developer</li>
+                <li>Anthony Robinson - Backend Developer</li>
+                <li>Ashleigh Sayers - Backend Developer</li>
+                <li>Jayden Dry - Backend Developer</li>
+              </ul>
+
+              <ul className={styles.front_line_team}>
+                <h1>Website Design</h1>
+                <li>Salma Jarrar - UX/UI Designer</li>
+                <li>Logan Trundle - UX/UI Designer</li>
+                <li>Cheyenne Toh - UX/UI Designer</li>
+                <li>Alishba Tausif - UX/UI Designer</li>
+                <li>Kate MacKenzie - UX/UI Designer</li>
+                <li>Nadifa Aziza - UX/UI Designer</li>
+                <li>Melina Lim - UX/UI Designer</li>
+                <li>Valentina Cendales - UX/UI Designer</li>
+                <li>Darcy Walters - UX/UI Designer</li>
+              </ul>
+
+              <ul className={styles.front_line_team}>
+                <h1>Showreel 2D Development</h1>
+                <li>Flynn Denby - 2D Concept Artist</li>
+                <li>Lewis Fuller Hill - 2D Concept Artist</li>
+                <li>Rein Aspinall - 2D Concept Artist</li>
+                <li>Mhairi Harris - 2D Concept Artist</li>
+                <li>Teo Zhui - 2D Concept Artist</li>
+                <li>Amber Howe - 2D Concept Artist</li>
+                <li>Simone Woolfitt - 2D Concept Artist</li>
+                <li>Johnson Nguyen - 2D Concept Artist</li>
+                <li>Lai H.C. - 2D Concept Artist</li>
+              </ul>
+            </div>
+
+            <div className={styles.column3}> {/* ---COLUMN3--- */}
+              <ul className={styles.front_line_team}>
+                <h1>Showreel 3D Development</h1>
+                <li>Macgregor Lawson - 3D Modeller and Sound Designer</li>
+                <li>Campbell McCracken - 3D Modeller and Rigger</li>
+                <li>Max Wright - 3D Modeller</li>
+                <li>Xavier Goh - 3D Modeller</li>
+                <li>Rizky Hefriadi - 3D Modeller</li>
+                <li>Taylor Ingvarson - 3D Modeller</li>
+                <li>Jeremy Ho - 3D Modeller</li>
+                <li>James Powell - 3D Modeller</li>
+                <li>Zoey Tan - 3D Modeller</li>
+                <li>Ori Elek - 3D Modeller</li>
+                <li>James Green - 3D Modeller</li>
+                <li>Scarlett Coley - 3D Modeller</li>
+                <li>Rebecca Gibson - 3D Modeller</li>
+                <li>Angelo B.N. Gregorio - 3D Modeller</li>
+                <li>Todd Huckle - 3D Modeller</li>
+                <li>Steven White-Smith - 3D Modeller and Texture Artist</li>
+                <li>Melanie Tan - Texture Artist</li>
+                <li>Hayley Zeller - Texture Artist and Rigger</li>
+              </ul>
+
+              <ul className={styles.front_line_team}>
+                <h1>Showreel Compositing</h1>
+                <li>Max Wright - 3D Animator</li>
+                <li>Campbell McCracken - 3D Animator</li>
+                <li>Steven White-Smith - 3D Animator and Unreal Engine Compositor</li>
+                <li>Macgregor Lawson - Video Compositor</li>
+                <li>Taylor Ingvarson - Motion Graphics</li>
+                <li>Rebecca Gibson - Editor</li>
+                <li>Angelo B.N. Gregorio - Editor</li>
+                <li>Scarlett Coley - Editor</li>
+              </ul>
+            </div>
         </div>
+
       </section>
 
 
