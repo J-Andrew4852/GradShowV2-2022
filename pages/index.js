22561ceef6cefc6d748ce99f81817849038a0445
J-Andrew4852 <james.andrew4852@gmail.com>
2022-11-19 19:41:31 +0800

    minor-err-fixes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script'
-import { version } from 'react'
 
 // COMPONENT IMPORTS ==================================================================== // 
 import animationShowreel from '../public/assets/images/Showreel1.png'
