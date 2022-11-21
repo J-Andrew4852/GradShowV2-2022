a792809b99b506cc907afc93376d4f314b2b3f3f
Rachael Moriconi <93764298+studiomorte@users.noreply.github.com>
2022-11-21 14:05:33 +0800

    navbar adjustments and layout correction

--- components/Navigation/Navbar/Navbar.js
+++ components/Navigation/Navbar/Navbar.js
@@ -52,69 +52,72 @@ export const Navbar = () => {
     hammy ? 
 
     <nav className={styles.container}>
-      <div className={`text-med ${styles.nav_elements}`}>   
-        <Link href={'/'} className={styles.logo}>
-          {/* <a> */}
-            <Image src={logo} alt="Exhibition Space logo"/>
-          {/* </a> */}
-        </Link>
-        <div className={styles.hammy} onClick={() => { setHammy(false); }}>
-          <div className={styles.crossTainer}>
-              <svg width="20" height="20" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <line x1="1" y1="15.9393" x2="15.9393" y2="1" stroke="#283618" stroke-width="1.5" stroke-linecap="round"/>
-                  <line x1="0.75" y1="-0.75" x2="21.8774" y2="-0.75" transform="matrix(-0.707107 -0.707107 -0.707107 0.707107 16 17)" stroke="#283618" stroke-width="1.5" stroke-linecap="round"/>
-              </svg>
+      <div className={styles.navcontainer}>
+        <div className={`text-med ${styles.nav_elements}`}>   
+          <Link href={'/'} className={styles.logo}>
+            {/* <a> */}
+              <Image src={logo} alt="Exhibition Space logo"/>
+            {/* </a> */}
+          </Link>
+          <div className={styles.hammy} onClick={() => { setHammy(false); }}>
+            <div className={styles.crossTainer}>
+                <svg width="20" height="20" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="1" y1="15.9393" x2="15.9393" y2="1" stroke="#283618" stroke-width="1.5" stroke-linecap="round"/>
+                    <line x1="0.75" y1="-0.75" x2="21.8774" y2="-0.75" transform="matrix(-0.707107 -0.707107 -0.707107 0.707107 16 17)" stroke="#283618" stroke-width="1.5" stroke-linecap="round"/>
+                </svg>
+            </div>
+          </div>
+
+          <div className={styles.nav_options}>
+            <Link href={'/'}>
+              <a className={router.pathname === '/' ? `link active` : `link`}>Home</a>
+            </Link>
+            <Link href={'/about'}>
+              <a className={router.pathname.startsWith('/about') ? `link active` : `link`}>About</a>
+            </Link>
+            <Link href={'/graduates'}>
+              <a className={router.pathname.startsWith('/graduates') ? `link active` : `link`}>Graduates</a>
+            </Link>
           </div>
         </div>
-
-        <div className={styles.nav_options}>
-          <Link href={'/'}>
-            <a className={router.pathname === '/' ? `link active` : `link`}>Home</a>
-          </Link>
-          <Link href={'/about'}>
-            <a className={router.pathname.startsWith('/about') ? `link active` : `link`}>About</a>
-          </Link>
-          <Link href={'/graduates'}>
-            <a className={router.pathname.startsWith('/graduates') ? `link active` : `link`}>Graduates</a>
-          </Link>
-        </div>
-
-        <div className={styles.mobile_nav_options}>
-          <Link href={'/'}>
-            <a className={router.pathname === '/' ? `link active` : `link`}>Home</a>
-          </Link>
-          <Link href={'/about'}>
-            <a className={router.pathname.startsWith('/about') ? `link active` : `link`}>About</a>
-          </Link>
-          <Link href={'/graduates'}>
-            <a className={router.pathname.startsWith('/graduates') ? `link active` : `link`}>Graduates</a>
-          </Link>
-        </div>
       </div>
+      <div className={styles.mobile_nav_options}>
+          <Link href={'/'}>
+            <a className={router.pathname === '/' ? `link active` : `link`}>Home</a>
+          </Link>
+          <Link href={'/about'}>
+            <a className={router.pathname.startsWith('/about') ? `link active` : `link`}>About</a>
+          </Link>
+          <Link href={'/graduates'}>
+            <a className={router.pathname.startsWith('/graduates') ? `link active` : `link`}>Graduates</a>
+          </Link>
+        </div>
     </nav>
 
     :
     <nav className={styles.container}>
-      <div className={`text-med ${styles.nav_elements}`}>   
-        <Link href={'/'} className={styles.logo}>
-          {/* <a> */}
-            <Image src={logo} alt="Exhibition Space logo"/>
-          {/* </a> */}
-        </Link>
-        
-        <div className={styles.hammy}  onClick={() => { setHammy(true); }}>
-          <Hamburger />
-        </div>
-        <div className={styles.nav_options}>
-          <Link href={'/'}>
-            <a className={router.pathname === '/' ? `link active` : `link`}>Home</a>
+      <div className={styles.navcontainer}>
+        <div className={`text-med ${styles.nav_elements}`}>   
+          <Link href={'/'} className={styles.logo}>
+            {/* <a> */}
+              <Image src={logo} alt="Exhibition Space logo"/>
+            {/* </a> */}
           </Link>
-          <Link href={'/about'}>
-            <a className={router.pathname.startsWith('/about') ? `link active` : `link`}>About</a>
-          </Link>
-          <Link href={'/graduates'}>
-            <a className={router.pathname.startsWith('/graduates') ? `link active` : `link`}>Graduates</a>
-          </Link>
+          
+          <div className={styles.hammy}  onClick={() => { setHammy(true); }}>
+            <Hamburger />
+          </div>
+          <div className={styles.nav_options}>
+            <Link href={'/'}>
+              <a className={router.pathname === '/' ? `link active` : `link`}>Home</a>
+            </Link>
+            <Link href={'/about'}>
+              <a className={router.pathname.startsWith('/about') ? `link active` : `link`}>About</a>
+            </Link>
+            <Link href={'/graduates'}>
+              <a className={router.pathname.startsWith('/graduates') ? `link active` : `link`}>Graduates</a>
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
