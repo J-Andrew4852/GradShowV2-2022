5df226e4ac79155e2de6a5203e20183982c3e4a8
J-Andrew4852 <james.andrew4852@gmail.com>
2022-11-24 00:41:06 +0800

    fatenir

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Telugu:wght@300;600&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.cdnfonts.com/css/avenir-lt-std" rel="stylesheet" />
+        {/* <link href="https://fonts.cdnfonts.com/css/avenir" rel="stylesheet" /> */}
         {/* <link rel="stylesheet" href="../styles/globals.css"></link> */}
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
