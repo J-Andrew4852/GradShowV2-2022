d7153db1c3169a4038876c4b95810c5ab391eb64
J-Andrew4852 <james.andrew4852@gmail.com>
2022-11-24 03:14:25 +0800

    ghosting

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
+        {/* TODO - get description to put into meta tag */}
+        <meta name="description" content="" />
+        <meta property="og:title" content="Exhibition Space || 2022 Curtin Grad Show"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://www.curtindesign.com/"/>
+        <meta property="og:title" content="Curtin Exhibition Space"/>
+        <meta property="og:site_name" content="Exhibition Space || 2022 Curtin Grad Show"/>
+        {/* TODO - get description to put into meta tag */}
+        <meta property="og:description" content=""/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="shortcut icon" href="../public/favicon.ico" />
-      <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-      <meta httpEquiv="Pragma" content="no-cache" />
-      <meta httpEquiv="Expires" content="0" />
+      <link rel="stylesheet" href="../styles/globals.css"></link>
     </Head>
     <Layout>
         <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,6 @@ export default function Document() {
     <Html>
       <Head>
         {/* Link for custom fonts (such as from google fonts to be included in this sectioAn) */}
-        {/* TODO - get description to put into meta tag */}
-        <meta name="description" content="" />
-        <meta property="og:title" content="Exhibition Space || 2022 Curtin Grad Show"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://www.curtindesign.com/"/>
-        <meta property="og:title" content="Curtin Exhibition Space"/>
-        <meta property="og:site_name" content="Exhibition Space || 2022 Curtin Grad Show"/>
-        {/* TODO - get description to put into meta tag */}
-        <meta property="og:description" content=""/>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
@@ -24,11 +14,8 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Telugu:wght@300;600&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet"></link>
         <link href="/fonts/avenir_ff/style.css" rel='stylessheet' />
         {/* <link href="https://fonts.cdnfonts.com/css/avenir-lt-std" rel="stylesheet" /> */}
-        <link href="https://fonts.cdnfonts.com/css/avenir" rel="stylesheet" />
+        {/* <link href="https://fonts.cdnfonts.com/css/avenir" rel="stylesheet" /> */}
         {/* <link rel="stylesheet" href="../styles/globals.css"></link> */}
-        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-        <meta httpEquiv="Pragma" content="no-cache" />
-        <meta httpEquiv="Expires" content="0" />
       </Head>
       <body>
         <Main />
